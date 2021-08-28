82d521971d4b7389730527abeefb08c5c3b14f1d
Alex <krounix@gmail.com>
2021-08-28 23:59:38 +0300

    исправил и доработал

--- task_2.py
+++ task_2.py
@@ -2,9 +2,20 @@ from pprint import pprint
 from task_1 import VACANCIES
 
 
-SALARY = int(input('Введите желаемую зарплату: '))
-for vac in VACANCIES.find({'$or': [
-        {'min_salary': {'$gt': SALARY}},
-        {'max_salary': {'$gt': SALARY}}
-]}):
-    pprint(vac)
+def get_good_job(salary):
+    """
+    Возвращает вакансии у которых минимальная зарплата выше указаной,
+    если указаны и минимальная и максимальная, но минимальная ниже, то эта вакансия не подходит,
+    если минимальная не указана, но з.п. "до" выше запрашиваемой, то эта вакансия попадет в вывод.
+    :param salary: сумма зарплаты
+    """
+    for vac in VACANCIES.find({'$or': [
+            {'min_salary': {'$gt': salary}},
+            {'max_salary': {'$gt': salary}}
+    ]}):
+        if vac.get('min_salary') and vac.get('min_salary') < salary:
+            continue
+        pprint(vac)
+
+
+get_good_job(int(input('Введите желаемую зарплату: ')))
